fab8e46c66da6ab50dfea88472a6fde17258d3b9
Matthias LECONTE <pixmat@gmail.com>
2017-01-20 10:26:41 +0100

    Boilerplate init.

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+console.log('mounting react app ...');  // eslint-disable-line no-console
+render(<App />, document.getElementById('__TODO__'));
+
